ed109f836249ee7712c48259e6fc8e8305b5a77e
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-23 16:23:47 -0300

    feat(RefundItem): add RefundItem component for displaying refund details

---
+++ src/components/RefundItem.tsx
@@ -0,0 +1,33 @@
+import { data } from "react-router"
+
+export type RefundItemProps = {
+    id: string
+    username: string
+    category: string
+    amount: string
+    categoryImg: string
+}
+
+type Props = React.ComponentProps<"a"> & {
+    data: RefundItemProps
+}
+
+
+export function RefundItem({ data, ...rest }: Props) {
+    return (
+        <a {...rest} className="flex items-center gap-3 hover:bg-green-100/5 cursor-pointer rounded-md p-2">
+            <img src={data.categoryImg} alt="Ícone da categoria" className="w-8 h-8"/>
+
+            <div className="flex flex-col flex-1">
+                <strong className="text-sm text-gray-100">{data.username}</strong>
+                <span className="text-xs text-gray-200">{data.category}</span>
+            </div>
+
+            <span className="text-sm text-gray-100 font-semibold">
+                <small className="font-normal text-gray-200">R$ </small>
+                {data.amount}
+            </span>
+        </a>
+        
+    )
+}
