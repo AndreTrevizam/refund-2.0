c8c9ecb086ee489529fda7b45e1d4020a90ba5e2
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-23 10:38:20 -0300

    refactor(Button): enhance button component with variant support and class merging

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,25 @@
+import { ClassMerge } from "../utils/classMerge"
+
 type Props = React.ComponentProps<"button"> & {
   isLoading?: boolean
+  variant?: "base" | "icon"
 }
 
-export function Button({ children, type = "button", isLoading, ...rest }: Props) {
+const variants = {
+  button: {
+    base: "h-12",
+    icon: "h-12 w-12"
+  }
+}
+
+export function Button({ children, className, type = "button", isLoading, variant = "base", ...rest }: Props) {
   return (
-    <button type={type} disabled={isLoading} {...rest} className="w-full h-12 bg-green-100 rounded-lg font-bold text-white text-sm cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-wait">
+    <button type={type} disabled={isLoading} className={ClassMerge([
+      "w-full bg-green-100 flex items-center justify-center rounded-lg font-bold text-white text-sm cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-wait",
+      variants.button[variant], className
+    ])}
+      {...rest}
+    >
       {children}
     </button>
   )
