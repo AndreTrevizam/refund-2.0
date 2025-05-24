dbbac2dbb0ff9827b55913d6d9c0b2a464bab0f7
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-24 11:30:58 -0300

    feat(Button): add 'iconSmall' variant for button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,21 +2,22 @@ import { ClassMerge } from "../utils/classMerge"
 
 type Props = React.ComponentProps<"button"> & {
   isLoading?: boolean
-  variant?: "base" | "icon"
+  variant?: "base" | "icon" | "iconSmall"
 }
 
 const variants = {
   button: {
     base: "h-12",
-    icon: "h-12 w-12"
+    icon: "h-12 w-12",
+    iconSmall: "h-8 w-8"
   }
 }
 
 export function Button({ children, className, type = "button", isLoading, variant = "base", ...rest }: Props) {
   return (
     <button type={type} disabled={isLoading} className={ClassMerge([
-      "w-full bg-green-100 flex items-center justify-center rounded-lg font-bold text-white text-sm cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-wait",
-      variants.button[variant], className
+      "w-full bg-green-100 flex items-center justify-center rounded-lg font-bold text-white text-sm cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50",
+      variants.button[variant], className, isLoading && "cursor-no-drop"
     ])}
       {...rest}
     >
