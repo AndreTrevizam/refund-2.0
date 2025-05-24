96a0715a82d0913ed2b174087460e4702dc27ddc
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-24 15:25:02 -0300

    feat(Routes): implement dynamic routing based on user role and add loading state

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,36 @@ import { BrowserRouter } from "react-router";
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
+import { Loading } from "../components/Loading";
+
+const isLoading = false
+
+const session = {
+  user: {
+    role: "employee"
+  }
+}
 
 export function Routes() {
+
+  function Route() {
+    switch (session.user.role) {
+      case "employee":
+        return <EmployeeRoutes/>
+      case "manager":
+        return <ManagerRoutes/>
+      default:
+        return <AuthRoutes/>
+    }
+  }
+
+  if (isLoading) {
+    return <Loading/>
+  }
+
   return (
   <BrowserRouter>
-    {/* <AuthRoutes></AuthRoutes> */}
-    {/* <EmployeeRoutes></EmployeeRoutes> */}
-    <ManagerRoutes></ManagerRoutes>
+    <Route></Route>
   </BrowserRouter>
   )
 }
