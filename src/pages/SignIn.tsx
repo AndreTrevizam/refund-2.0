a1e812864f0ec1db738d893f24f24cd8b652c7ce
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-25 16:14:29 -0300

    feat(SignIn): refactor sign-in logic to use action state and Zod validation

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,22 +1,55 @@
 import { Button } from "../components/Button"
 import { Input } from "../components/Input"
-import { useState } from "react"
+import { useActionState } from "react"
+import { z, ZodError } from "zod"
+import { api } from "../services/api"
+import { Axios, AxiosError } from "axios"
+
+const signInSchema = z.object({
+  email: z.string().email({ message: "E-mail inválido" }),
+  password: z.string().trim().min(1, "Informe a senha")
+})
 
 export function SignIn() {
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
+  const [state, formAction, isLoading] = useActionState(signIn, null)
 
-  function onSubmit(e: React.FormEvent) {
-    e.preventDefault()
+  async function signIn(_: any, formData: FormData) {
+
+    try {
+      const data = signInSchema.parse({
+        email: formData.get("email"),
+        password: formData.get("password")
+      })
+
+      const response = await api.post("/sessions", data)
+      console.log(response.data)
+
+      
+    } catch (error) {
+      console.log(error)
+
+      if (error instanceof ZodError) {
+        return { message: error.issues[0].message }
+      }
+
+      if (error instanceof AxiosError) {
+        return { message: error.response?.data.message }
+      }
+
+      return { message: "Não foi possível entrar" }
+    }
   }
 
   return (
-    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
-      <Input required legend="E-mail" type="email" placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)}></Input>
+    <form action={formAction} className="w-full flex flex-col gap-4">
+      <Input name="email" required legend="E-mail" type="email" placeholder="seu@email.com"></Input>
 
-      <Input required legend="Senha" type="password" placeholder="senhasupersegura" onChange={(e) => setPassword(e.target.value)}></Input>
+      <Input name="password" required legend="Senha" type="password" placeholder="senhasupersegura"></Input>
+
+      <p className="text-sm text-red-600 text-center my-2 font-medium">
+        {state?.message}
+      </p>
 
       <Button type="submit" isLoading={isLoading}>Entrar</Button>
 
